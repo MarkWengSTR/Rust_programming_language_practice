3b2e8d3a212dbada787ae0f61b3ee4b53e83f082
Mark <bskin0330@gmail.com>
2021-03-11 11:12:29 +0800

    Study ownership concept

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,17 +1,25 @@
 fn main() {
-    let s = String::from("gogo");
+    let s = "hello world";
 
-    println!("{}", first_word(&s));
+    let word = first_word(&s);
+
+    println!("{}", word);
+
+    let s2 = String::from("hello world");
+
+    let word2 = first_word(&s2);
+    println!("{}", word2);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
+
